c7571a9a864954db43ad64552963badbc281755c
ho333ein <ho333einshokrollahi@gmail.com>
2019-04-21 19:28:14 +0430

    Update mtprotoproxy.py

--- mtprotoproxy.py
+++ mtprotoproxy.py
@@ -280,8 +280,6 @@ class LayeredStreamReaderBase:
     def __init__(self, upstream):
         self.upstream = upstream
 
-    async def read(self, n):
-        return await self.upstream.read(n)
 
     async def readexactly(self, n):
         return await self.upstream.readexactly(n)
